91bbf4a0ef7d01a7911f7d48e9c7deb5ed20a3fb
Andy000002 <52848853+Andy000002@users.noreply.github.com>
2020-11-25 02:23:06 +0800

    first

---
+++ backtrack.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int arr[5];
+
+void backtracking(int n){
+    if(n==5){    //如果陣列0~4格每格的值設定完後就輸出答案
+        for(int i=0;i<5;i++)
+            printf("%d",arr[i]);
+        printf("\n");
+        return;
+    }
+    else{
+        for(int i=0;i<10;i++){
+            arr[n]=i;    //設定第n項的值為i
+            backtracking(n+1);
+        }
+    }
+}
+int main(){
+    backtracking(0);
+    return 0;
+}
